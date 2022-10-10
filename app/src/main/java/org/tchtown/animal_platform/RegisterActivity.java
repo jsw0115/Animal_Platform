65bc2a0fec3f2a7ffb5b6e67ea433dae82f16e52
jsw0115 <jsw0101151@naver.com>
2022-10-10 16:44:34 +0900

    2022-10-10 PM4 FireBase연동 실패 -> 나중에 다시 확인 필요 UI 마이페이지, 로그인, 회원가입 생성 완

--- app/src/main/java/org/tchtown/animal_platform/RegisterActivity.java
+++ app/src/main/java/org/tchtown/animal_platform/RegisterActivity.java
@@ -3,6 +3,8 @@ package org.tchtown.animal_platform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        findViewById(R.id.registerButton).setOnClickListener(onClickListener);
     }
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.registerButton:
+                    Log.e("클릭","클릭");
+                    break;
+            }
+        }
+    };
 }
