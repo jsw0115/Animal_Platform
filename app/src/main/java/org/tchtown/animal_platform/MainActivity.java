84d6f3ea22e4af14d51e9049dcda293321d79b12
baewhaApp <baewha1234@gmail.com>
2022-12-03 03:37:06 +0900

    2022-12-03 AM 0336 회원가입 기능 구현 중

--- app/src/main/java/org/tchtown/animal_platform/MainActivity.java
+++ app/src/main/java/org/tchtown/animal_platform/MainActivity.java
@@ -1,11 +1,14 @@
 package org.tchtown.animal_platform;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -66,6 +69,25 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
         //*/
+
+
+        // 2022-12-03 AM 0319
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+        }
+
     }
 
 }
--- app/src/main/java/org/tchtown/animal_platform/RegisterActivity.java
+++ app/src/main/java/org/tchtown/animal_platform/RegisterActivity.java
@@ -3,26 +3,76 @@ package org.tchtown.animal_platform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = "RegisterActivity";
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
         findViewById(R.id.registerButton).setOnClickListener(onClickListener);
+
     }
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             switch (view.getId()){
                 case R.id.registerButton:
-                    Log.e("클릭","클릭");
+                    signUp();
                     break;
             }
         }
     };
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+    }
+    private void signUp(){
+        String email = ((EditText)findViewById(R.id.EmailEditText)).getText().toString();
+        String password = ((EditText)findViewById(R.id.editPasswordText)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // UI 성공 updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            // UI 실패
+                        }
+                    }
+                });
+
+    }
+
+
 }
